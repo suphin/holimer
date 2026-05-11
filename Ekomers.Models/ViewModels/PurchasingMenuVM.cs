6e0b8c4952375302c210a7e6fb6dda0baefe865e
Suphi Nohutlu <suphi.n@hotmail.com>
2026-05-11 18:45:40 +0300

    11052026 iş 1807

---
+++ Ekomers.Models/ViewModels/PurchasingMenuVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ekomers.Models.ViewModels
+{
+	public class PurchasingMenuVM
+	{
+		public int ToplamTalep { get; set; }
+
+		public int TaslakTalep { get; set; }
+		public int OnayBekleyenTalep { get; set; }
+
+		public int OnaylananTalep { get; set; }
+
+		public int TeklifAsamasında { get; set; }
+		public int OnayBekleyenTeklif { get; set; }
+
+		public int OnaylananTeklif { get; set; }
+	}
+}
---
+++ Ekomers.Web/Component/PurchasingMenu.cs
@@ -0,0 +1,46 @@
+﻿using Ekomers.Data.Services;
+using Ekomers.Data.Services.IServices;
+using Ekomers.Models.Ekomers;
+using Ekomers.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Ekomers.Web.Component
+{
+	using Ekomers.Data;
+	using Ekomers.Models.Enums;
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.EntityFrameworkCore;
+
+	public class PurchasingMenuViewComponent : ViewComponent
+	{
+		private readonly ApplicationDbContext _context;
+
+		public PurchasingMenuViewComponent(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var model = new PurchasingMenuVM();
+
+			// Talep Sayıları
+			model.ToplamTalep = await _context.Request.Where(a=> a.IsActive==true && a.IsDelete==false).CountAsync();
+
+			model.TaslakTalep = await _context.Request
+				.CountAsync(x => x.DurumID == (int)EnumRequestDurum.Taslak);	
+
+			model.OnayBekleyenTalep = await _context.Request
+				.CountAsync(x => x.DurumID == (int)EnumRequestDurum.OnayBekliyor);
+
+			// Teklif Sayıları
+			model.OnayBekleyenTeklif = await _context.RequestUrunler
+				.CountAsync(x => x.OfferDurumID == (int)EnumOfferDurum.TeklifOnayBekliyor);
+			model.TeklifAsamasında = await _context.RequestUrunler
+				.CountAsync(x => x.OfferDurumID == (int)EnumOfferDurum.TeklifAsamasinda);
+
+			return View(model);
+		}
+	}
+}
